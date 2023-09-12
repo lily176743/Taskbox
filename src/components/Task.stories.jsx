37959875dfdfed7d434e44b57166c5b568cd306a
BrianMartin176743 <lily176743@gmail.com>
2023-09-13 05:13:04 +0900

    final

--- src/components/Task.stories.jsx
+++ src/components/Task.stories.jsx
@@ -1,5 +1,7 @@
 import Task from './Task';
 
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
 export default {
   component: Task,
   title: 'Task',
@@ -30,6 +32,15 @@ export const Archived = {
     task: {
       ...Default.args.task,
       state: 'TASK_ARCHIVED',
+    },
+  },
+};
+
+export const LongTitle = {
+  args: {
+    task: {
+      ...Default.args.task,
+      title: longTitleString,
     },
   },
 };
